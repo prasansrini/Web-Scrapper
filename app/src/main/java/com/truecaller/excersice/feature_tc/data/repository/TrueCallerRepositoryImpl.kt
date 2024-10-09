8d96959460b1da95469c3d0fc89c0c67454b4804
Prasanna Srinivasan <github@prasansrini.com>
2024-10-09 15:59:17 +0200

    Code cleanup - stable

--- app/src/main/java/com/truecaller/excersice/feature_tc/data/repository/TrueCallerRepositoryImpl.kt
+++ app/src/main/java/com/truecaller/excersice/feature_tc/data/repository/TrueCallerRepositoryImpl.kt
@@ -5,8 +5,6 @@ import com.truecaller.network.repository.ApiOperation
 import com.truecaller.network.repository.TrueCallerApiClient
 import javax.inject.Inject
 
-private const val TAG = "TrueCallerRepositoryImp"
-
 class TrueCallerRepositoryImpl @Inject constructor(
     private val client: TrueCallerApiClient
 ) : TrueCallerRepository {
--- app/src/main/java/com/truecaller/excersice/feature_tc/ui/theme/Color.kt
+++ app/src/main/java/com/truecaller/excersice/feature_tc/ui/theme/Color.kt
@@ -2,18 +2,11 @@ package com.truecaller.excersice.feature_tc.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val ColorPrimary = Color(0xff262d3a)
 val ColorAction = Color(0xffb5eefd)
-val ColorSurface = Color(0xffead7ce)
-val ColorTextPrimary = Color(0xffffffff)
-val ColorTextSecondary = Color(0xffead7ce)
 
 val blue = Color(70, 130, 180)
